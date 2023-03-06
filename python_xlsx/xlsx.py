d8190bfbef3a11339f622f928d1cb620a458c14f
xmmzslla <wppq20011206@gmail.com>
2023-03-07 08:57:50 +0900

    판다스 연결

--- python_xlsx/xlsx.py
+++ python_xlsx/xlsx.py
@@ -1,12 +1,6 @@
 import pandas as pd
-import os
+exdt = pd.read_excel(r"C:\k_digital\python_workspace\python_xlsx\데이터 가공.xlsx")
 
-#방법 1. 전체 경로 입력
-df1=pd.read_excel('C:\k_digital\1.project\데이터 가공')
-
-
-#방법 2. 작업경로 지정 후 파일 이름 입력
-# os.chdir('C:\k_digital\1.project')
-
-# df1=pd.read_excel('데이터 가공')
+print(exdt.head(6))
+# https://real-bell.tistory.com/73
 
