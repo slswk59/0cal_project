7bc9c859ca0f4e3b6261664f3029e703b309a290
xmmzslla <wppq20011206@gmail.com>
2023-03-12 23:51:08 +0900

    python

--- python_xlsx/Algorithm/test.py
+++ python_xlsx/Algorithm/test.py
@@ -8,20 +8,34 @@ exdt = exdt[['아이디.1', '상품수량.1', '상품코드.1']]
 print(exdt)
 
 # 아이디별로 구매한 상품 코드와 수량을 그룹화
-exdt_grouped = exdt.groupby(['아이디.1', '상품코드.1']).sum()
-print(exdt_grouped)
+exdt_data = exdt[['아이디.1', '상품코드.1', '상품수량.1']]
+exdt_data = exdt_data.groupby(['아이디.1', '상품코드.1']).sum()
 
-# 각 아이디별로 가장 많이 구매한 상품 코드
-exdt_max = exdt_grouped['상품수량.1'].groupby(level=0).idxmax()
+exdt_id = exdt_data.reset_index()[['아이디.1', '상품코드.1']]
+exdt_id = exdt_id.drop_duplicates()
+
+exdt_merge = pd.merge(exdt_id, exdt_data, on=['아이디.1', '상품코드.1'])
+print(exdt_merge)
+
+# exdt_grouped = exdt.groupby(['아이디.1', '상품코드.1']).sum()
+# print(exdt_grouped)
+
+# 아이디 별, 가장많이구매한상품코드 top3추출
+exdt_max = exdt_merge.groupby(['아이디.1']).apply(lambda x: x.nlargest(3, '상품수량.1')).reset_index(drop=True)
+exdt_max = exdt_max.groupby(['아이디.1'])['상품코드.1'].apply(list).reset_index(name='가장많이구매한상품코드')
 print(exdt_max)
 
-# bbini1109 아이디와 많이 구매한 상품 코드에 대한 상관관계 계산
-bbini_corr = exdt_corr.loc['bbini1109']
+# xmmzslla님과 많이 구매한 상품 코드
+xmmz_corr = exdt_corr.loc['xmmzslla']
 
-# 다른 아이디들과의 상관관계 계산 후, 상위 3개 아이디 구하기
-top_3_corr = bbini_corr.drop('bbini1109').nlargest(3)
-top_3_ids = top_3_corr.index.tolist()
+# xmmzslla님과 구매 상품 코드에 대한 상관관계가 높은 아이디 3개 구하기
+corr_top3 = exdt_corr[xmmz_corr.index].corrwith(xmmz_corr).sort_values(ascending=False)[1:4].index.tolist()
+print(corr_top3)
 
-print(top_3_ids)
+
+
+
+
+
 
 
