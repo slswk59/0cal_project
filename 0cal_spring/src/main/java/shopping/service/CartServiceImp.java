4a4698cd8519226bd323cd947f383350cce8da6a
YOONJUNG HWANG <brownkaki99@gmail.com>
2023-03-22 09:36:06 +0900

    장바구니 추가 및 수정 구현 완료

---
+++ 0cal_spring/src/main/java/shopping/service/CartServiceImp.java
@@ -0,0 +1,56 @@
+package shopping.service;
+
+import java.util.List;
+
+import shopping.dao.CartDAO;
+import shopping.dto.CartDTO;
+
+public class CartServiceImp implements CartService{
+
+	private CartDAO cartDto;
+	
+	public CartServiceImp() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void setCartDto(CartDAO cartDto) {
+		this.cartDto = cartDto;
+	}
+
+	@Override
+	public void insertCartProcess(CartDTO dto) {
+		// TODO Auto-generated method stub
+		cartDto.insertCart(dto);
+	}
+
+	@Override
+	public int countCartProcess(String id, int pr_key) {
+		// TODO Auto-generated method stub
+		return cartDto.countCart(id, pr_key);
+	}
+
+	@Override
+	public void updateCartProcess(CartDTO dto) {
+		cartDto.updateCart(dto);
+	}
+
+	@Override
+	public List<CartDTO> listCartProcess(String id) {
+		// TODO Auto-generated method stub
+		return cartDto.listCart(id);
+	}
+
+	@Override
+	public void deleteCartProcess(int cart_key) {
+		// TODO Auto-generated method stub
+		cartDto.deleteCart(cart_key);
+	}
+
+	@Override
+	public void modifyCartProcess(CartDTO dto) {
+		cartDto.modifyCart(dto);
+		
+	}
+	
+	
+}
